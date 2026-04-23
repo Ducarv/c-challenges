39cfdbdce3c9242608053231ecb10b5e61f76c2c
Eduardo Carvalho Silva <educarvalho009@gmail.com>
2026-04-23 07:19:55 -0400

    invert array

---
+++ invertor-array.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+  int arr[5] = {1, 2, 3, 4, 5};
+
+  for(int i = 0; i <= (4 / 2); i++) {
+    int temp = arr[i];
+    arr[i] = arr[4 - i];
+    arr[4 - i] = temp;
+  }
+
+  for(int i = 0; i < 5; i++) {
+    printf("%d", arr[i]);
+  }
+
+  printf("\n");
+
+  return 0;
+}
